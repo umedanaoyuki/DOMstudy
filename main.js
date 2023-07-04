8ec0e4b5d37dc2de3e0013bf6aad5ebde03cc4fa
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-04 23:06:57 +0900

    toggle

--- main.js
+++ main.js
@@ -4,11 +4,13 @@
 {
     //ボタン要素取得後 addEventListner
     document.querySelector('button').addEventListener('click',() => {
-        if (document.querySelector('p').classList.contains('pink-bg') == false) {
-            document.querySelector('p').classList.add('pink-bg');
-        } else {
-            document.querySelector('p').classList.remove('pink-bg');
-        }
+        // if (document.querySelector('p').classList.contains('pink-bg') == false) {
+        //     document.querySelector('p').classList.add('pink-bg');
+        // } else {
+        //     document.querySelector('p').classList.remove('pink-bg');
+        // }
+        document.querySelector('p').classList.toggle('pink-bg');
+
     });
 
     console.log('Hello');
