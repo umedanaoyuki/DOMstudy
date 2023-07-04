99d618631a7da7386585ae233351f24e30c04ac0
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-04 22:53:57 +0900

    querySelector

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 'use strict';
 
+//プログラムが実行される順序を考えてみる
 {
     //ボタン要素取得後 addEventListner
     document.querySelector('button').addEventListener('click',() => {
-        console.log('test');
+        //document.querySelector('p').textContent = 'こんにちは';
+        //console.log('test');
+        document.querySelector('p').textContent = document.querySelector('button').textContent;
     });
+
+    console.log('Hello');
 
 }
