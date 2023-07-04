83da54d120005827b06c44aa33096ff486dce4f0
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-04 23:42:20 +0900

    appendChild, remove, confirmなど

--- main.js
+++ main.js
@@ -4,23 +4,12 @@
 {
     //ボタン要素取得後 addEventListner
     document.querySelector('button').addEventListener('click',() => {
-        // if (document.querySelector('p').classList.contains('pink-bg') == false) {
-        //     document.querySelector('p').classList.add('pink-bg');
-        // } else {
-        //     document.querySelector('p').classList.remove('pink-bg');
-        // }
-        // document.querySelectorAll('li')[0].textContent = 'Changed';
-        // document.querySelectorAll('li')[1].textContent = 'Changed';
-        // document.querySelectorAll('li')[2].textContent = 'Changed';
+        const liElement = document.createElement('li');
+        liElement.textContent = 'Hanako';
+        document.querySelector('ul').appendChild(liElement); 
 
-        document.querySelector('#second').textContent = 'Yahoo';
-        document.querySelectorAll('.target').forEach((li) => {
-            li.textContent = 'Changed';
-
+        if (confirm('Are you Sure?') === true) {
+        document.querySelector('#second').remove();}
         })
-
-    });
-
     console.log('Hello');
-
-}
+};
