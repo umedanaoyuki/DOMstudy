6f9cecd50ac19de4cb0e05d43d3a604a81e47729
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-04 23:47:18 +0900

    input textareaにはvalueでアクセス

--- main.js
+++ main.js
@@ -4,12 +4,7 @@
 {
     //ボタン要素取得後 addEventListner
     document.querySelector('button').addEventListener('click',() => {
-        const liElement = document.createElement('li');
-        liElement.textContent = 'Hanako';
-        document.querySelector('ul').appendChild(liElement); 
-
-        if (confirm('Are you Sure?') === true) {
-        document.querySelector('#second').remove();}
-        })
-    console.log('Hello');
+        //alert(document.querySelector('input').value);
+        alert(document.querySelector('textarea').value);
+    })
 };
