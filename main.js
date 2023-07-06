05611c57e1a34d2cb84b0f3e438b39ce4f4d369d
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-06 23:05:16 +0900

    fucus, blur

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 
 //プログラムが実行される順序を考えてみる
 {
-    document.querySelector('input').addEventListener('input',() => {
-        const pElement = document.querySelector('p');
-        const inputElement = document.querySelector('input');
-        pElement.textContent = inputElement.value.length;
+    document.querySelector('input').addEventListener('focus',() => {
+        document.querySelector('p').textContent = 'English Only'
+    });
 
+    document.querySelector('input').addEventListener('blur',() => {
+        document.querySelector('p').textContent = '';
+    });    
 
-        });
+    document.querySelector('input').focus();    
+
 }
